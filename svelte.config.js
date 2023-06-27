31553ce92ecfc3a51cfc1242e42aa0d26655e43d
charlyflynn <35696585+charlyflynn@users.noreply.github.com>
2023-06-27 12:37:55 +0100

    install scss and config

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
 import { default as staticAdapter } from '@sveltejs/adapter-static';
 import { default as autoAdapter } from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+	preprocess: [vitePreprocess(), preprocess({ typescript: true, scss: true })],
 	kit: {
 		adapter: process.env.env === 'gh-pages' ? staticAdapter() : autoAdapter()
 	},
